2cc059673faad39a5c899c6ae64d61199e5a0373
Marcos Conceição <mconceicao@protonmail.com>
2021-05-18 22:19:30 -0300

    Nova Cor + Clipboard Browser

--- i3/scripts/mybar.sh
+++ i3/scripts/mybar.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-# cat -n /tmp/jjClipboardManager.history | tac | tr -d ' ' | grep -m2 '_#_' | cut -d '	' -f1 > /tmp/abc ; ini=`cat /tmp/abc | tail -1`; end=`cat /tmp/abc | head -1` ; ini=$((ini-1)); end=$((end-1)); echo 'sed -n '${ini}','${end}'p' /tmp/jjClipboardManager.history | sh | grep -v '_#_' | grep . > /tmp/lastPaste
-
-function my_clipboard()
-{
-  cat -n /tmp/jjClipboardManager.history | tac | tr -d ' ' | grep -m2 '_#_' | cut -d '	' -f1 > /tmp/abc ;
-  ini=`cat /tmp/abc | tail -1`;
-  end=`cat /tmp/abc | head -1` ;
-  ini=$((ini-1));
-  end=$((end-1));
-  echo 'sed -n '${ini}','${end}'p' /tmp/jjClipboardManager.history | sh | grep -v '_#_' | grep . > /tmp/lastPaste
-
- if [ -f "/tmp/lastPaste" ];
- then
-    tail -1 /tmp/lastPaste |sed 's/"//g' | cut -c 1-20
- else
-  echo 'nada no clipboard'
- fi
-
-}
-
 function vpnConnection()
 {
   local running=`ps -ef | grep -i vpn | grep LEADCOMM_SP_Remote_Access_mconceicao.p12 | wc -l `
@@ -27,9 +7,9 @@ function vpnConnection()
   echo $running >> /tmp/saida.txt
   if [ $running  = '1' ]
   then
-    echo '  - '$(ip r | grep tun0 | grep src | grep --color -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | grep -Ev '0$')
+    echo '  - '$(ip r | grep tun0 | grep src | grep --color -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | grep -Ev '0$')
   else
-    echo '  Leadcomm -VPN - Disconnected'
+    echo '  Leadcomm - VPN - Disconnected'
   fi
 }
 
@@ -44,40 +24,14 @@ echo '[]'
 do
   echo ',['
 
-  echo ' {"background": "#","name":"data","full_text":"  '$(date)'"}'
-  echo ',{"background": "#","name":"clipboard","full_text":"ClipBoard''"}'
-  echo ',{"background": "#","name":"windows52","full_text":"'VM .52'"}'
-  echo ',{"background": "#","name":"windows60","full_text":"'VM .60'"}'
-  echo ',{"background": "#","name":"vpn","full_text":"'$(vpnConnection)'"}'
-  echo ',{"background": "#","name":"rede","full_text":" '$(iw dev | sed -nr 's/^\t\tssid (.*)/\1/p') ' - '$(ip route get 1 | sed -n "s/.*src \([0-9.]\+\).*/\1/p")'"}'
-  echo ',{"background": "#","name":"bateria","full_text":" '$(cat /sys/class/power_supply/BAT0/status) '-' $(cat /sys/class/power_supply/BAT0/capacity)%'"}'
-  echo ',{"background": "#","name":"cpu","full_text":" '$(LC_NUMERIC=en_US.UTF-8 top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}')'"}'
-  echo ',{"background": "#","name":"memoria","full_text":" '$(free -g | awk '/^Mem/ {print $3}')''/''$(free -g | awk '/^Mem/ {print $2}' )'"}'
-  echo ',{"background": "#","name":"volume","full_text":"'$(awk -F"[][]" '/dB/ { print $2 }' <(amixer sget Master))'"  }'
-
-
-
-
-  # echo ' {"background": "#ff5555","name":"data","full_text":"  '$(date)'"}'
-  # echo ',{"background": "#e5c07b","name":"clipboard","full_text":"ClipBoard''"}'
-  # echo ',{"background": "#FFFFFF","name":"windows52","full_text":"'VM .52'"}'
-  # echo ',{"background": "#FFFFF6","name":"windows60","full_text":"'VM .60'"}'
-  # echo ',{"background": "#56b6c2","name":"vpn","full_text":"'$(vpnConnection)'"}'
-  # echo ',{"background": "#50fa7b","name":"rede","full_text":" '$(iw dev | sed -nr 's/^\t\tssid (.*)/\1/p') ' - '$(ip route get 1 | sed -n "s/.*src \([0-9.]\+\).*/\1/p")'"}'
-  # echo ',{"background": "#f1fa8c","name":"bateria","full_text":" '$(cat /sys/class/power_supply/BAT0/status) '-' $(cat /sys/class/power_supply/BAT0/capacity)%'"}'
-  # echo ',{"background": "#bd93f9","name":"cpu","full_text":" '$(LC_NUMERIC=en_US.UTF-8 top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}')'"}'
-  # echo ',{"background": "#ff79c6","name":"memoria","full_text":" '$(free -g | awk '/^Mem/ {print $3}')''/''$(free -g | awk '/^Mem/ {print $2}' )'"}'
-  # echo ',{"background": "#8be9fd","name":"volume","full_text":"'$(awk -F"[][]" '/dB/ { print $2 }' <(amixer sget Master))'"  }'
-
-
-
-
-
-
-
-
-
-
+  echo ' {"background": "#ABB2BF" , "name":"data","full_text":"  '$(date)'"}'
+  echo ',{"background": "#ABB2BF" , "name":"clipboard","full_text":"ClipBoard''"}'
+  echo ',{"background": "#ABB2BF" , "name":"vpn","full_text":"'$(vpnConnection)'"}'
+  echo ',{"background": "#ABB2BF" , "name":"rede","full_text":" '$(iw dev | sed -nr 's/^\t\tssid (.*)/\1/p') ' - '$(ip route get 1 | sed -n "s/.*src \([0-9.]\+\).*/\1/p")'"}'
+  echo ',{"background": "#ABB2BF" , "name":"bateria","full_text":" '$(cat /sys/class/power_supply/BAT0/status) '-' $(cat /sys/class/power_supply/BAT0/capacity)%'"}'
+  echo ',{"background": "#ABB2BF" , "name":"cpu","full_text":" '$(LC_NUMERIC=en_US.UTF-8 top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}')'"}'
+  echo ',{"background": "#ABB2BF" , "name":"memoria","full_text":" '$(free -g | awk '/^Mem/ {print $3}')''/''$(free -g | awk '/^Mem/ {print $2}' )'"}'
+  echo ',{"background": "#ABB2BF" , "name":"volume","full_text":"'$(awk -F"[][]" '/dB/ { print $2 }' <(amixer sget Master))'"  }'
 
   echo ']'
 
@@ -89,48 +43,15 @@ while read line;
 do
   name=`echo $line | sed 's/^,//g ' | jq .name | sed 's/"//g'`
 
-
   case $name in
       clipboard )
-      cat /tmp/jjClipboardManager.history > /tmp/1.txt
-      gnome-terminal -e 'gedit /tmp/1.txt' & ;;
+      tac /tmp/jjClipboardManager.history.txt > /tmp/1.txt
+      brave-browser --newtab "file:////tmp/1.txt"  ;;
+
 
       volume    ) gnome-control-center -s "Sound"   & ;;
       rede      )
             echo "Ak2539yz" | xsel --clipboard --input 
            gnome-terminal -e 'nmtui' & ;;
-
-
-      windows60 )
-
-        isRunning=$(ps -ef | grep rdesktop | wc -l)
-
-        echo $isRunning >> /tmp/marcos1
-
-        if [ "$isRunning" -gt "1" ]
-        then
-          ps -ef | grep rdesktop | awk '{ print $2 }' | xargs kill -9
-        else
-          gnome-terminal -e 'bash /home/mconceicao/.win.60.sh &'
-        fi
-      ;;
-
-
-
-
-      windows52 )
-
-        isRunning=$(ps -ef | grep rdesktop | wc -l)
-
-        echo $isRunning >> /tmp/marcos
-
-        if [ "$isRunning" -gt "1" ]
-        then
-          ps -ef | grep rdesktop | awk '{ print $2 }' | xargs kill -9
-        else
-          gnome-terminal -e 'bash /home/mconceicao/.win.52.sh &'
-        fi
-      ;;
   esac
-
 done
